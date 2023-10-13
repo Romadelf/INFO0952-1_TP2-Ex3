34c08946037c1782d2b161a06a99cb56c3b85391
Roman Del Favero <roman.delfavero@student.uliege.be>
2023-10-13 23:48:16 +0200

    encrypt/decrypt

--- crypto.c
+++ crypto.c
@@ -12,7 +12,7 @@ Image* mapImage(int nbImg, Image **images, Pixel (*f)(int nb, Pixel*),
         int newImage) {
 
     // temp Image* p = getImagePixel(images[0], getNbRows(images[0]), getNbRows(images[0]));
-    Pixel* p = malloc(sizeof(Pixel));
+    Pixel *p = malloc(sizeof(Pixel));
     if (f(nbImg, p).r == 1)
         return images[newImage];
 
@@ -22,18 +22,41 @@ Image* mapImage(int nbImg, Image **images, Pixel (*f)(int nb, Pixel*),
 
 Image* decrypt(Image *img) {
 
-    return img;
-    // p2 = (p1 % 8) * 32
+    size_t rows = getNbRows(img);
+    size_t cols = getNbColumns(img);
+    Image *img2 = createImage(rows, cols);
+
+    for (size_t i = 0; i < cols; i++)
+        for (size_t j = 0; j < rows; j++) {
+            Pixel p = getImagePixel(img, j, i);
+
+            p.r = p.r % 8 * 32;
+            p.g = p.g % 8 * 32;
+            p.b = p.b % 8 * 32;
+
+            setImagePixel(img2, j, i, p);
+        }
+
+    return img2;
 
 }
 
+/** TODO : Les images sont supposées de même taille */
 void encrypt(Image *img1, Image *img2) {
 
-    printf("%d", (int) getNbRows(img1) + (int) getNbRows(img2));
-    printf("\n");
-    printf("2,3 : %d", getImagePixel(img1, 2, 3).r);
-    printf("\n");
+    size_t rows = getNbRows(img1);
+    size_t cols = getNbColumns(img1);
 
-    // p1 = p1 - p1 % 8 + p2 / 32
+    for (size_t i = 0; i < cols; i++)
+        for (size_t j = 0; j < rows; j++) {
+            Pixel p1 = getImagePixel(img1, j, i);
+            Pixel p2 = getImagePixel(img2, j, i);
+
+            p1.r -= p1.r % 8 + p2.r / 32;
+            p1.g -= p1.g % 8 + p2.g / 32;
+            p1.b -= p1.b % 8 + p2.b / 32;
+
+            setImagePixel(img1, j, i, p1);
+        }
 
 }
